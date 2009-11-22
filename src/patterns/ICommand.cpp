e5d1607cfca7a2faa439d8d770c951f7e631b471
Tobias Frost <coldtobi@users.sourceforge.net>
2009-11-22 12:21:49 +0100

    ICommand gets a map for its parameters
    It showed, during the async-comm patch, that it is hard
    to keep track on the parameters to a ICommand, if we want
    to supply more than one information (like success/error and on
    success also the data)
    So now ICommand gets a std::map<std::string,boost::any> to keep track
    of data more easily.

--- src/patterns/ICommand.cpp
+++ src/patterns/ICommand.cpp
@@ -1,29 +1,28 @@
 /* ----------------------------------------------------------------------------
-   solarpowerlog
-   Copyright (C) 2009  Tobias Frost
+ solarpowerlog
+ Copyright (C) 2009  Tobias Frost
 
-   This file is part of solarpowerlog.
+ This file is part of solarpowerlog.
 
-   Solarpowerlog is free software; However, it is dual-licenced
-   as described in the file "COPYING".
+ Solarpowerlog is free software; However, it is dual-licenced
+ as described in the file "COPYING".
 
-   For this file (ICommand.cpp), the license terms are:
+ For this file (ICommand.cpp), the license terms are:
 
-   You can redistribute it and/or modify it under the terms of the GNU
-   General Public License as published by the Free Software Foundation; either
-   version 3 of the License, or (at your option) any later version.
+ You can redistribute it and/or modify it under the terms of the GNU
+ General Public License as published by the Free Software Foundation; either
+ version 3 of the License, or (at your option) any later version.
 
-   This programm is distributed in the hope that it will be useful, but
-   WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   Lesser General Public License for more details.
+ This programm is distributed in the hope that it will be useful, but
+ WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
 
-   You should have received a copy of the GNU Library General Public
-   License along with this proramm; if not, see
-   <http://www.gnu.org/licenses/>.
-   ----------------------------------------------------------------------------
-*/
-
+ You should have received a copy of the GNU Library General Public
+ License along with this proramm; if not, see
+ <http://www.gnu.org/licenses/>.
+ ----------------------------------------------------------------------------
+ */
 
 /** \file ICommand.cpp
  *
@@ -39,31 +38,46 @@
 #include "patterns/ICommandTarget.h"
 
 /** Just the constructor taking all elements... **/
-ICommand::ICommand(int command, ICommandTarget *target, void *dat)
-{
-	cmd = command; trgt = target; data = dat;
+ICommand::ICommand(int command, ICommandTarget *target, void *dat) {
+	cmd = command;
+	trgt = target;
+	data = dat;
+}
+
+ICommand::ICommand(int command, ICommandTarget *target, std::map<std::string,
+		boost::any> dat) {
+
+}
+
+ICommand::ICommand(int command, ICommandTarget *target) {
 }
 
 /** Destructor, even for no need for destruction */
-ICommand::~ICommand()
-{
+ICommand::~ICommand() {
 }
 
 /** Delegate the command to the one that should do the work */
-void ICommand::execute()
-{
+void ICommand::execute() {
 	trgt->ExecuteCommand(this);
 }
 
 /** Getter for the private cmd field (field is for Commandees use) */
-int ICommand::getCmd() const
-{
+int ICommand::getCmd() const {
 	return cmd;
 }
 
 /** Getter for the private cmd field (field is for Commandees use) */
-void *ICommand::getData() const
-{
+void *ICommand::getData() const {
 	return data;
+}
+
+const boost::any ICommand::findData(const std::string &key)
+		throw (std::invalid_argument) {
+
+	std::map<std::string, boost::any>::iterator it = dat.find(key);
+	if (it != dat.end()) {
+		return (*it).second;
+	}
+	throw(std::invalid_argument(key));
 }
 
--- src/patterns/ICommand.h
+++ src/patterns/ICommand.h
@@ -1,35 +1,36 @@
 /* ----------------------------------------------------------------------------
-   solarpowerlog
-   Copyright (C) 2009  Tobias Frost
+ solarpowerlog
+ Copyright (C) 2009  Tobias Frost
 
-   This file is part of solarpowerlog.
+ This file is part of solarpowerlog.
 
-   Solarpowerlog is free software; However, it is dual-licenced
-   as described in the file "COPYING".
+ Solarpowerlog is free software; However, it is dual-licenced
+ as described in the file "COPYING".
 
-   For this file (ICommand.h), the license terms are:
+ For this file (ICommand.h), the license terms are:
 
-   You can redistribute it and/or  modify it under the terms of the GNU Lesser
-   General Public License (LGPL) as published by the Free Software Foundation;
-   either version 3 of the License, or (at your option) any later version.
+ You can redistribute it and/or  modify it under the terms of the GNU Lesser
+ General Public License (LGPL) as published by the Free Software Foundation;
+ either version 3 of the License, or (at your option) any later version.
 
-   This programm is distributed in the hope that it will be useful, but
-   WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   Lesser General Public License for more details.
+ This programm is distributed in the hope that it will be useful, but
+ WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
 
-   You should have received a copy of the GNU Library General Public
-   License along with this proramm; if not, see
-   <http://www.gnu.org/licenses/>.
-   ----------------------------------------------------------------------------
-*/
-
+ You should have received a copy of the GNU Library General Public
+ License along with this proramm; if not, see
+ <http://www.gnu.org/licenses/>.
+ ----------------------------------------------------------------------------
+ */
 
 /** \file ICommand.h
  *
  *  Created on: May 17, 2009
  *      Author: tobi
  */
+
+#define DEPRECIATE_OLD_ICOMMAND
 
 #ifndef ICOMMAND_H_
 #define ICOMMAND_H_
@@ -38,7 +39,11 @@
 #include "config.h"
 #endif
 
-// #include "patterns/ICommandTarget.h"
+#include <string>
+#include <map>
+#include <stdexcept>
+
+#include <boost/any.hpp>
 
 class ICommandTarget;
 
@@ -54,25 +59,73 @@ class ICommandTarget;
  */
 class ICommand {
 public:
-	ICommand(int command, ICommandTarget *target, void *dat = 0);
+	ICommand(int command, ICommandTarget *target, std::map<std::string,
+			boost::any> dat);
+
+	ICommand(int command, ICommandTarget *target);
 
 	virtual ~ICommand();
 
 	void execute();
 
-    int getCmd() const;
+	int getCmd() const;
 
-    void *getData() const;
-    void setData(void *data)
-    {
-        this->data = data;
-    }
+#ifdef DEPRECIATE_OLD_ICOMMAND
+private:
+#endif
+	ICommand(int command, ICommandTarget *target, void *dat);
+	void *getData() const;
+	void setData(void *data) {
+		this->data = data;
+	}
+
+#ifdef DEPRECIATE_OLD_ICOMMAND
+public:
+#endif
+
+
+	/** Find Data in Command
+	 *
+	 * Returns the data associated in the map to the given key
+	 *
+	 * \in param key to search for
+	 * \returns boost::any object with the data
+	 *
+	 * \throw std::invalid_argument { throws this if data is not existant. The
+	 * data of the invalid_argument is the key which was not found }
+	 */
+	const boost::any findData(const std::string &key)
+			throw (std::invalid_argument);
+
+	/** Remove Data from Command
+	 *
+	 * Removes the named key from the data list of the command.
+	 * \note: As the underlaying storage is a std::map,
+	 * the associated boost::any object will be deleted.
+	 */
+	inline void RemoveData(const std::string &key) {
+		dat.erase(key);
+	}
+
+	/** Add/Replace Data from the Command
+	 *
+	 * Add new data, or if the data is already existing, replace
+	 * the data with the new one.
+	 *
+	*/
+	void addData(const std::string &key, const boost::any &data) {
+		if (dat.count(key)) {
+			dat.erase(key);
+		}
+		dat.insert(std::pair<std::string, boost::any>(key, data));
+	}
 
 private:
 	int cmd;
 	ICommandTarget *trgt;
 	void *data;
 
+	std::map<std::string, boost::any> dat;
 
 };
 

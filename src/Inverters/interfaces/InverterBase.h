7b7e098205fa923caf00d6f1afed8ea60a217b5c
Tobias Frost <coldtobi@users.sourceforge.net>
2015-01-03 16:27:37 +0100

    [IInverterBase] new API to generate CConfigCentral object.

--- src/Inverters/interfaces/InverterBase.h
+++ src/Inverters/interfaces/InverterBase.h
@@ -1,20 +1,20 @@
 /* ----------------------------------------------------------------------------
  solarpowerlog -- photovoltaic data logging
 
-Copyright (C) 2009-2012 Tobias Frost
+ Copyright (C) 2009-2015 Tobias Frost
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU Lesser General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Lesser General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU Lesser General Public License for more details.
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Lesser General Public License for more details.
 
-    You should have received a copy of the GNU Lesser General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ You should have received a copy of the GNU Lesser General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
  ----------------------------------------------------------------------------
  */
@@ -206,6 +206,7 @@ Copyright (C) 2009-2012 Tobias Frost
 #include "configuration/ILogger.h"
 
 class ICapaIterator;
+class CConfigCentral;
 
 using namespace std;
 
@@ -276,6 +277,16 @@ public:
 	virtual IConnect * getConnection(void) const {
 		return connection;
 	}
+
+	/** Create & Get a CCOnfigCentral object for this instance.
+	 *
+	 * @return generated object. Ownwership is waived to caller.
+	 *
+	 * \note in overriden version, call the base's implementation.
+	 * The base *might* alraead have added properties to check aloing with
+	 * their descriptions.
+	 */
+	virtual CConfigCentral* getConfigCentralObject(void);
 
 protected:
 	/// Add a Capability for the inverter.
